091aa92376c6cbeae3242b54446ead32fc628b25
Hetal Rachh <36734792+hetalrachh@users.noreply.github.com>
2020-04-04 19:16:46 +0530

    Delete App.js

--- public/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CalculatorBuilder from "./containers/CalculatorBuilder";
-
-//root component
-function App() {
-  return <CalculatorBuilder />;
-}
-
-export default App;
